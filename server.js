10dc4838f0d26a07c886c1bedfe3822b7e966bed
rmnaser90 <rmnaser90@gmail.com>
2020-12-01 22:21:02 +0200

    hello world

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
 const express = require('express')
 const app = express()
+const path = require('path')
+
+
+app.use(express.static(path.join(__dirname,'dist')))
+app.use(express.static(path.join(__dirname,'node_modules')))
 
 
 
 
-
-
-
-
-
-
+app.use()
 
 
 
