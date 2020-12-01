6e138a02564f5c424b27db83516f63417d33b4aa
rmnaser90 <rmnaser90@gmail.com>
2020-12-01 22:02:58 +0200

    npm init and instal express mongoose and axios

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const app = express()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const port = 3000
+app.listen(port, function () {
+    console.log(`Running on port ${port}`)
+})
